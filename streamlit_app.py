d19a3eaa758d53582c399ceb7487d6492c61c4be
hegerjl12 <42073527+hegerjl12@users.noreply.github.com>
2022-04-06 20:03:08 -0700

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,15 @@
 import streamlit as st
 
+st.set_page_config(
+     page_title="Login Expirations",
+     page_icon="🔒",
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
+
+st.title('Login Expirations')
+
+with st.container():
+    st.header('SIPR')
+    
+    sipr_date = st.date_input()
